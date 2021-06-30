1fa98d5863102aafc46f3a2407b51f681eac3990
bambangnibos <86288411+bambangnibos@users.noreply.github.com>
2021-06-30 12:26:11 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RL3uUGv6um8Bq7nNq4TTkvk4FQaxbQDagg.go -p x --cpu 2
